b5ee186d2d5ef5a0c22defd535189d954d0eae22
Hugo Arenari <hugoarenari@gmail.com>
2020-11-09 06:38:54 -0300

    migrations e map model

---
+++ src/database/migrations/1604912224178-create_transactions.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+// eslint-disable-next-line @typescript-eslint/class-name-casing
+export default class createTransactions1604912224178
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'double precision',
+          },
+          {
+            name: 'type',
+            type: 'enum',
+            enum: ['income', 'outcome'],
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
+}
--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,21 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
 class Category {
+  @PrimaryGeneratedColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
